59ba751852c7998a79d42fcbbfd9a7e7761d33fb
Helen <aquercus@gmail.com>
2021-01-27 21:00:44 -0600

    some renames

---
+++ createInstance.sh
@@ -0,0 +1,2 @@
+#! /bin/bash
+aws cloudformation create-stack --stack-name new-instance-stack --template-body file://new-instance.yaml
--- createVPConly.sh
+++
@@ -1,2 +0,0 @@
-#! /bin/bash
-aws cloudformation create-stack --stack-name vpc-only-stack --template-body file://vpc-only.yaml
